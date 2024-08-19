83f599b52fbd3ccb52fd17245ad02967fae8813d
Rafa Bernal <calmayourdrama@proton.me>
2024-08-19 17:58:21 +0200

    return code router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { Routes } from '@angular/router'
-import DashboardLayoutComponent from './feature/dashboard/layout.component'
 
 export default [
-  // {
-  //   path: 'dashboard',
-  //   loadComponent: () => import('./feature/dashboard/layout.component').then(({ default: dashboard }) => dashboard),
-  // },
   {
-    path: '',
-    component: DashboardLayoutComponent,
-    // redirectTo: '/dashboard',
-    // pathMatch: 'full',
+    path: 'dashboard',
+    loadComponent: () => import('./feature/dashboard/layout.component').then(({ default: dashboard }) => dashboard),
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
   },
 ] as Routes
