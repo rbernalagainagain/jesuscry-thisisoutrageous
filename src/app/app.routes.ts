0394abd0eb87c25ac8584c67c528bc64039cd80e
Rafa Bernal <calmayourdrama@proton.me>
2024-08-19 18:10:58 +0200

    test router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router'
 
 export default [
   {
-    path: 'dashboard',
+    path: '',
     loadComponent: () => import('./feature/dashboard/layout.component').then(({ default: dashboard }) => dashboard),
   },
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full',
-  },
+  // {
+  //   path: '',
+  //   redirectTo: '/dashboard',
+  //   pathMatch: 'full',
+  // },
 ] as Routes
